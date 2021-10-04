cad6f3f832813107cc63ba747619b2f215f5c6bc
RodrigoAranda <rodrigo.aranda.mt@correo.usa.edu.co>
2021-10-04 09:45:03 -0500

    primer commit desarrollo trth

--- src/ciclo3/Ciclo3.java
+++ src/ciclo3/Ciclo3.java
@@ -15,7 +15,7 @@ public class Ciclo3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Usuario u = new Usuario();
     }
     
 }
