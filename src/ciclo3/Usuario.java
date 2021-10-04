1c2d7f095aafb0a727f36b580057fb3eb4c0a157
'RodrigoAanda' <'rarandamt@correo.usa.edu.co'>
2021-10-04 09:31:35 -0500

    primer commit desarrollo

---
+++ src/ciclo3/Usuario.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclo3;
+
+/**
+ *
+ * @author RAF
+ */
+public class Usuario {
+     private String nombre;
+    private String apellido;
+    private int documento;
+
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(int documento) {
+        this.documento = documento;
+    }
+    
+    
+}
