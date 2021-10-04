2c1a1316b3d738f4c0b89bc98d07fab180818217
RodrigoAranda <rodrigo.aranda.mt@correo.usa.edu.co>
2021-10-04 10:52:07 -0500

    Se adicionan los tributos de las clases

--- src/ciclo3/Matricula.java
+++ src/ciclo3/Matricula.java
@@ -10,5 +10,44 @@ package ciclo3;
  * @author RAF
  */
 public class Matricula {
+    private int numeroMatricula;
+    private String fecha;
+    private double valorMatricula;
+
+    public Matricula(int numeroMatricula, String fecha, double valorMatricula) {
+        this.numeroMatricula = numeroMatricula;
+        this.fecha = fecha;
+        this.valorMatricula = valorMatricula;
+    }
+    
+    
+    
+    
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(int numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getValorMatricula() {
+        return valorMatricula;
+    }
+
+    public void setValorMatricula(double valorMatricula) {
+        this.valorMatricula = valorMatricula;
+    }
+    
+    
     
 }
